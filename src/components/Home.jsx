7d83b60eec3d316bfd8179a1dd17743bd4d75035
teli <7telita@gmail.com>
2023-11-07 16:43:30 -0600

    nav and search routes

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    <div>
+        <p>
+            Hi
+        </p>
+    </div>
+
+}
+
+export default Home;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => (
+    <Nav>
+         <nav className="main-nav">
+        <ul>
+          <li><a href='#'>Cats</a></li>
+          <li><a href='#'>Dogs</a></li>
+          <li><a href='#'>Computers</a></li>
+        </ul>
+      </nav> 
+    </Nav>
+
+);
+
+export default Nav;
