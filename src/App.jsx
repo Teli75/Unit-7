6c410ad7bdddba67e78be11ab9e5ee1bc4dd97f4
teli <7telita@gmail.com>
2023-11-09 18:34:25 -0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,55 +16,48 @@ function App() {
   const [tennisPhotos, setTennisPhotos] = useState([]);
   const [pickleBallPhotos, setPickleBallPhotos] = useState([]);
 
+  useEffect(() => {
+    if (tableTennisPhotos.length === 0) fetchData("Table Tennis");
+    if (tennisPhotos.length === 0) fetchData("Tennis");
+    if (pickleBallPhotos.length === 0) fetchData("Pickle Ball");
+  }, []);
 
- useEffect( () => {
-  if (tableTennisPhotos.length === 0)
-    fetchData("Table Tennis");
-if (tennisPhotos.length === 0)
-   fetchData("Tennis");
-if (pickleBallPhotos.length === 0)
-    fetchData("Pickle Ball");  
- }, [])
+  function fetchData(query) {
+    axios
+      .get(
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+      )
+      .then((response) => {
+        if (query === "Table Tennis") {
+          setTableTennisPhotos(response.data.photos.photo);
+        } else {
+          setPhotos(response.data.photos.photo);
+        }
+        if (query === "Tennis") {
+          setTennisPhotos(response.data.photos.photo);
+        } else {
+          setPhotos(response.data.photos.photo);
+        }
+        if (query === "Pickle Ball") {
+          setPickleBallPhotos(response.data.photos.photo);
+        } else {
+          setPhotos(response.data.photos.photo);
+        }
+        console.log(photos);
+      })
+      .catch((error) => {
+        console.log("Error fetching data", error);
+      });
+  }
 
-  function fetchData( query ) {
-      axios
-        .get(
-          `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
-        )
-        .then((response) => {
-          if (query ===  "Table Tennis" ){
-            setTableTennisPhotos(response.data.photos.photo);
-          } else {
-          setPhotos(response.data.photos.photo);
-          }
-          if (query ===  "Tennis" ){
-            setTennisPhotos(response.data.photos.photo);
-          } else {
-          setPhotos(response.data.photos.photo);
-          }
-          if (query ===  "Pickle Ball" ){
-            setPickleBallPhotos(response.data.photos.photo);
-          } else {
-          setPhotos(response.data.photos.photo);
-          }
-          console.log(photos);
-        })
-        .catch((error) => {
-          console.log("Error fetching data", error);
-        });
-    }
-
-  const handleQueryChange = ( title ) => {
-    setQuery( title );
-    fetchData( query );
+  const handleQueryChange = (title) => {
+    setQuery(title);
+    fetchData(query);
   };
 
   return (
     <div className="container">
-
-      <Search
-        callFetchData = { fetchData }
-      />
+      <Search callFetchData={fetchData} />
 
       <Nav />
 
@@ -73,19 +66,38 @@ if (pickleBallPhotos.length === 0)
 
         <Route
           path="/tabletennis"
-          element={<PhotoList title="Table Tennis" data={photos} queryState={query} changeQuery={ handleQueryChange }  />}
+          element={
+            <PhotoList
+              title="Table Tennis"
+              data={photos}
+              queryState={query}
+              changeQuery={handleQueryChange}
+            />
+          }
         />
         <Route
           path="/pickleball"
-          element={<PhotoList title="Pickle Ball" data={photos} queryState={query} changeQuery={ handleQueryChange } />}
+          element={
+            <PhotoList
+              title="Pickle Ball"
+              data={photos}
+              queryState={query}
+              changeQuery={handleQueryChange}
+            />
+          }
         />
         <Route
           path="/tennis"
-          element={<PhotoList title="Tennis" data={photos} queryState={query} changeQuery={ handleQueryChange } />}
+          element={
+            <PhotoList
+              title="Tennis"
+              data={photos}
+              queryState={query}
+              changeQuery={handleQueryChange}
+            />
+          }
         />
-        <Route 
-        path="/search/:query" 
-        element={<PhotoList data={photos} />} />
+        <Route path="/search/:query" element={<PhotoList data={photos} />} />
       </Routes>
     </div>
   );
